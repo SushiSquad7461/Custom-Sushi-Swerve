9204a05214a1f99c31663439effea7f950ad0c2f
Daniel Gorbatov <dzgorbatov@gmail.com>
2022-07-07 12:16:45 -0700

    add offest

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -11,6 +11,7 @@ import com.ctre.phoenix.sensors.CANCoder;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.kSwerveModule;
+import SushiFrcLib.Math.Conversion;
 import SushiFrcLib.Math.Rotation2;
 import SushiFrcLib.Math.Vector2;
 import SushiFrcLib.Motor.MotorHelper;
@@ -19,14 +20,16 @@ public class SwerveModule extends SubsystemBase {
   private final WPI_TalonFX drive;
   private final WPI_TalonFX turn;
   private final CANCoder canCoder;
+  private final double angleOffest;
 
   private double newAngle;
   private boolean turnToNewAngle = false;
 
-  public SwerveModule(int driveCanId, TalonFXInvertType driveInversion,int turnCanId, int canCoderPort) {
+  public SwerveModule(int driveCanId, TalonFXInvertType driveInversion,int turnCanId, int canCoderPort, double angleOffest) {
     drive = MotorHelper.createFalconMotor(driveCanId, kSwerveModule.DRIVE_CURRENT_LIMIT, driveInversion);
     turn = MotorHelper.createFalconMotor(turnCanId, kSwerveModule.TURN_CURRENT_LIMIT, TalonFXInvertType.CounterClockwise);
     canCoder = new CANCoder(canCoderPort);
+    this.angleOffest = angleOffest;
 
     drive.config_kP(0, kSwerveModule.kPDrive);
     drive.config_kI(0, kSwerveModule.kIDrive);
@@ -34,8 +37,12 @@ public class SwerveModule extends SubsystemBase {
     drive.config_kF(0, kSwerveModule.kFDrive);
   }
 
+  public double getModuleAngle() {
+    return Conversion.normalizeAngle(canCoder.getPosition() - angleOffest);
+  }
+
   public void updateModule(Vector2 newPos) {
-    if (Math.abs(newPos.getAngle().toDegrees() - canCoder.getPosition()) >= kSwerveModule.ERROR_BOUND) {
+    if (Math.abs(newPos.getAngle().toDegrees() - getModuleAngle()) >= kSwerveModule.ERROR_BOUND) {
         turnToNewAngle = true;
         newAngle = newPos.getAngle().toDegrees();
     } else {
@@ -48,7 +55,7 @@ public class SwerveModule extends SubsystemBase {
   @Override
   public void periodic() {
     if (turnToNewAngle) {
-        if (Math.abs(newAngle - canCoder.getPosition()) <= kSwerveModule.ERROR_BOUND) {
+        if (Math.abs(newAngle - getModuleAngle()) <= kSwerveModule.ERROR_BOUND) {
             turnToNewAngle = false;
         } else if ((newAngle - canCoder.getPosition()) > 0) {
             turn.set(ControlMode.PercentOutput, kSwerveModule.TURN_MOTOR_SPEED);
